c078f703637a4c067e8f087e043fe2c74eee5506
Komal Saif <komalsaif296@gmail.com>
2025-06-05 08:31:25 -0700

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type'],
   credentials: false
 }));
 
